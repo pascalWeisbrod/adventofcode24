cfbbff4949571c1e2de7ae32ade4d52d662f8dfa
Pascal Weisbrod <pascal.weisbrod@tum.de>
2024-12-03 10:51:59 +0100

    day3 complete

--- code/day3/day3.cpp
+++ code/day3/day3.cpp
@@ -6,22 +6,43 @@ using namespace std;
 
 void work(const string& input)
 {
-    regex expression ("^mul\\([0-9]+,[0-9]+\\)");
     long multiSums = 0;
+    long multiCondSums = 0;
+    bool enabled = true;
+
+    regex expression ("^mul\\([0-9]+,[0-9]+\\)");
+    regex enabledExpr ("^((do\\(\\))|(don't\\(\\))|(mul\\([0-9]+,[0-9]+\\)))");
+
     for (int i = 0; i < input.size(); i++)
     {
         string current = input.substr(i, input.size() - i);
-        if(regex_search(current, expression))
+        if(regex_search(current, enabledExpr))
         {
-            int open = current.find("(") + 1;
-            int close = current.find(")") - open;
-            string parant = current.substr(open, close);
+            if (current.at(0) == 'm')
+            {
+                int open = current.find("(") + 1;
+                int close = current.find(")") - open;
+                string parant = current.substr(open, close);
 
-            int comma = parant.find(",");
-            int x = stoi(parant.substr(0, comma));
-            int y = stoi(parant.substr(comma + 1, parant.size() - comma - 1));
-            multiSums += x * y;
+                int comma = parant.find(",");
+                int x = stoi(parant.substr(0, comma));
+                int y = stoi(parant.substr(comma + 1, parant.size() - comma - 1));
+
+                multiSums += x * y;
+                if (enabled)
+                {
+                    multiCondSums += x * y;
+                }
+                continue;
+            }
+            if (current.at(2) == '(')
+            {
+                enabled = true;
+                continue;
+            }
+            enabled = false;
         }
     }
-    cout << multiSums << endl;
+    cout << "Multiplier: " << multiSums << endl;
+    cout << "Multiplier under condition: " << multiCondSums << endl;
 }
